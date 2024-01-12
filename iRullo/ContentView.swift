8f5972d77eb45e0148430335d6c8799c2a86c0f8
phdafoe <phdafoe@gmail.com>
2024-01-12 20:10:26 +0100

    Initial Commit

---
+++ iRullo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iRullo
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iRullo/iRulloApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRulloApp.swift
+//  iRullo
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iRulloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
